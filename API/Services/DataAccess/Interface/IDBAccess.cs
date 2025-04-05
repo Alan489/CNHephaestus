d72cdbc586f5d9af77208824c4a46599bf0fc913
Kaplan <t.kaplan.lcso@gmail.com>
2025-04-05 15:39:32 -0400

    SQLLite access

--- API/Services/DataAccess/Interface/IDBAccess.cs
+++ API/Services/DataAccess/Interface/IDBAccess.cs
@@ -4,7 +4,7 @@ namespace API.Services.DataAccess.Interface
 {
  public interface IDBAccess
  {
-  public DataTable Get(string query);
+  public DataTable? Get(string query);
   public bool Insert(string tableName, DataTable dt);
 
  }

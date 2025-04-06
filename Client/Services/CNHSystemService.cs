7a9e4fbe2d4c0a998cfab1ad0e789f955969d45b
Alan <1423adecowski@gmail.com>
2025-04-06 19:28:05 -0400

    Finish the main layout, start work on Windows

--- Client/Services/CNHSystemService.cs
+++ Client/Services/CNHSystemService.cs
@@ -1,5 +1,8 @@
 ﻿using CNHephaestus.Objects.CNHService;
+using CNHephaestus.Pages.Components.Windows;
+using Microsoft.AspNetCore.Components.Web;
 using Shared.CNH.Shared.Communication.Authentication;
+using System.ComponentModel;
 
 namespace CNHephaestus.Services
 {
@@ -30,6 +33,13 @@ namespace CNHephaestus.Services
    }
   }
 
+  public List<WindowBase> ActiveWindows { get; set; } = new List<WindowBase>();
+
+  public WindowBase? ActiveWindow { get; set; }
+
+  public event EventHandler<MouseEventArgs> rightClickContextMenu;
+  public event EventHandler<EventArgs> WindowsChanged;
+
   public CNHSystemService(ProxyService ps)
   {
    _proxyService = ps;
@@ -49,6 +59,16 @@ namespace CNHephaestus.Services
    int responseCode = await request.SendRequest();
 
    return request.Response;
+  }
+
+  public void FireRightClick(MouseEventArgs mea)
+  {
+   rightClickContextMenu?.Invoke(this, mea);
+  }
+
+  public void FireRefresh()
+  {
+   WindowsChanged?.Invoke(this, EventArgs.Empty);
   }
 
   public async Task<bool> initAuthenticate(string url, string username, string password)

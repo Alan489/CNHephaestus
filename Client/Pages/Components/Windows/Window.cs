f0553cf28c2e717f372995d36c5d90ba2e34daff
Alan <1423adecowski@gmail.com>
2025-04-06 21:50:27 -0400

    Update logic to Windows

---
+++ Client/Pages/Components/Windows/Window.cs
@@ -0,0 +1,17 @@
+﻿namespace CNHephaestus.Pages.Components.Windows
+{
+ public class Window
+ {
+  public string Title { get; set; }
+  public string Type { get; set; }
+  public List<string>? Data { get; set; }
+  public List<string> Memory { get; private set; } = new List<string>();
+
+  public Window(string type, string title, List<string>? data = null)
+  {
+   Title = title;
+   Type = type;
+   Data = data;
+  }
+ }
+}
